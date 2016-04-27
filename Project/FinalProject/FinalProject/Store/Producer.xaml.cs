71ea8380ddb120000bd8c680853790e458936287
unknown <jakezro00@aim.com>
2016-04-27 18:35:27 -0400

    updated producer more

--- Project/FinalProject/FinalProject/Store/Producer.xaml.cs
+++ Project/FinalProject/FinalProject/Store/Producer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,26 +13,26 @@ namespace FinalProject
     {
         private readonly Store _store;
         private readonly List<Stock> _stocks = new List<Stock>();
+        private readonly List<IBook> _unreleasedBooks = new List<IBook>(); 
         public Producer(Store s)
         {
             InitializeComponent();
             _store = s;
             Show();
+            InitBooks();
         }
 
         private void AddToStoreButton_Click(object sender, RoutedEventArgs e)
         {
-            AddBookToStore(null);
+            IBook Book = _unreleasedBooks[NotInStoreBox.SelectedIndex];
+            _unreleasedBooks.Remove(Book);
+            AddBookToStore(Book);
         }
 
-        public void AddToUnreleasedBox(IBook book)
+        private void InitBooks()
         {
-            this.NotInStoreBox.Items.Add(book.GetTitle());
-        }
-
-        private void InitUnreleasedBooks()
-        {
-            _stocks.Add(new Stock(
+            // UNRELEASED BOOKS
+            _unreleasedBooks.Add(
                 new Book(
                     new Image(),
                     "Harry Potter",
@@ -41,11 +42,9 @@ namespace FinalProject
                     "1234-51234",
                     "Me.Awesome",
                     new decimal(10))
-                )
                 );
-            //_store.AddBook(_stocks[0]);
 
-            _stocks.Add(new Stock(
+            _unreleasedBooks.Add(
                 new Book(
                     new Image(),
                     "The Hunger Games",
@@ -55,11 +54,9 @@ namespace FinalProject
                     "4321-62145",
                     "GoodPub",
                     new decimal(8))
-                )
                 );
 
-            _stocks.Add(new Stock(
-                new Book(
+            _unreleasedBooks.Add(new Book(
                     new Image(),
                     "Paper Towns",
                     "Sappy story",
@@ -68,27 +65,88 @@ namespace FinalProject
                     "3214-76582",
                     "OneStory",
                     new decimal(12.50))
+                );
+
+            // RELEASED BOOKS
+            _stocks.Add(new Stock(
+                new Book(
+                    new Image(),
+                    "The Martian",
+                    "Man goes to space, grows potatoes, does well for himself",
+                    "Andy Weir",
+                    "Adventure",
+                    "6543-98765",
+                    "Publishing Co.",
+                    new decimal(6.99))
+                )
+                );
+
+            _stocks.Add(new Stock(
+                new Book(
+                    new Image(),
+                    "To Kill a Mockingbird",
+                    "Good book",
+                    "Harper Lee",
+                    "Drama",
+                    "5576-09876",
+                    "NiceGuy",
+                    new decimal(10.00))
                 )
                 );
 
             for (int i = 0; i < _stocks.Count; i++)
             {
                 _store.AddBook(_stocks[i]);
-                AddToUnreleasedBox(_stocks[i].GetBook());
+                InStoreBox.Items.Add(_stocks[i].GetBook().GetTitle());
             }
 
-
-
-        }
-
-        private void InitReleasedBooks()
-        {
-
+            for (int i = 0; i < _unreleasedBooks.Count; i++)
+            {
+                NotInStoreBox.Items.Add(_unreleasedBooks[i].GetTitle());
+            }
         }
 
         public void AddBookToStore(IBook book)
         {
             _store.AddBook(_stocks.First(s => s.GetBook().EqualsBook(book)));
+        }
+
+        private void UpdateQuantityButton_Click(object sender, RoutedEventArgs e)
+        {
+            // number to update the book by
+            int updateNum = 0;
+
+            // make sure a selection in the list has been made
+            if (InStoreBox.SelectedIndex != -1)
+            {
+                // grab the user's input
+                string updateString = Microsoft.VisualBasic.Interaction.InputBox("Please enter a number to update the selected book's quantity", 
+                    _stocks[InStoreBox.SelectedIndex].GetBook().GetTitle(), "", -1, -1);
+                try
+                {
+                    // try using the user's input as the update number
+                    updateNum = Int32.Parse(updateString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Not a valid integer");
+                }
+
+                // get the current book
+                int i = InStoreBox.SelectedIndex;
+
+                if (updateNum > 0)
+                {
+                    _stocks[i].UpdateQuanitity(updateNum);
+                }
+                else
+                {
+                    Console.WriteLine("Update number was not > 0");
+                }
+
+                //Console.WriteLine(_stocks[i].GetQuantity());
+
+            }
         }
     }
 }
